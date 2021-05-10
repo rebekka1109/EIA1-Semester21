b647830c4e7fc69bb90647c903ed4b6894d5ca22
rebekka1109 <80899236+rebekka1109@users.noreply.github.com>
2021-05-10 13:03:22 +0200

    Add files via upload

--- Steckbrief/Aufgabe7.js
+++ Steckbrief/Aufgabe7.js
@@ -1,61 +1,26 @@
-var sounds = [];
-sounds[0] = new Audio("A.mp3");
-sounds[1] = new Audio("C.mp3");
-sounds[2] = new Audio("F.mp3");
-sounds[3] = new Audio("G.mp3");
-sounds[4] = new Audio("hihat.mp3");
-sounds[5] = new Audio("kick.mp3");
-sounds[6] = new Audio("laugh-1.mp3");
-sounds[7] = new Audio("laugh-2.mp3");
-sounds[8] = new Audio("snare.mp3");
-document.querySelector(".drumpad").addEventListener("click", function (e) {
-    var target = e.target;
-    switch (target.id) {
-        case "drum1":
-            playSample(0);
-            break;
-        case "drum2":
-            playSample(1);
-            break;
-        case "drum3":
-            playSample(2);
-            break;
-        case "drum4":
-            playSample(3);
-            break;
-        case "drum5":
-            playSample(4);
-            break;
-        case "drum6":
-            playSample(5);
-            break;
-        case "drum7":
-            playSample(6);
-            break;
-        case "drum8":
-            playSample(7);
-            break;
-        case "drum9":
-            playSample(8);
-            break;
-        default:
-            break;
-    }
-});
-function playSample(x) {
-    sounds[x].play();
+function playSample(sounds) {
+    var sound = new Audio(sounds);
+    sound.play();
 }
-var beat = [];
-beat[0] = new Audio("kick.mp3");
-beat[1] = new Audio("snare.mp3");
-beat[2] = new Audio("hihat.mp3");
-var myIntervall;
-document.querySelector("#playbutton").addEventListener("click", function () {
-    definedBeat();
+var beats = ["kick.mp3", "kick.mp3", "snare.mp3", "kick.mp3", "hihat.mp3"];
+var index = 0;
+document.querySelector('#drum1').addEventListener('click', function () { playSample("kick.mp3"); });
+document.querySelector('#drum2').addEventListener('click', function () { playSample("snare.mp3"); });
+document.querySelector('#drum3').addEventListener('click', function () { playSample("hihat.mp3"); });
+document.querySelector('#drum4').addEventListener('click', function () { playSample("F.mp3"); });
+document.querySelector('#drum5').addEventListener('click', function () { playSample("G.mp3"); });
+document.querySelector('#drum6').addEventListener('click', function () { playSample("A.mp3"); });
+document.querySelector('#drum7').addEventListener('click', function () { playSample("C.mp3"); });
+document.querySelector('#drum8').addEventListener('click', function () { playSample("laugh-2.mp3"); });
+document.querySelector('#drum9').addEventListener('click', function () { playSample("laugh-1.mp3"); });
+document.querySelector('#playbutton').addEventListener('click', function () {
+    var interval = setInterval(function () {
+        var beat = new Audio(beats[index]);
+        beat.play();
+        index = index + 1;
+        if (index == 5) {
+            index = 0;
+        }
+    }, 300);
 });
-function definedBeat() {
-    beat[0].play();
-    beat[1].play();
-    myIntervall = setInterval(definedBeat, 500);
-}
 //# sourceMappingURL=Aufgabe7.js.map
